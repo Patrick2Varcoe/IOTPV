d3eccf421ace9bc04144c3f747af591998018a91
Patrick2Varcoe <patrick2.varcoe@live.uwe.ac.uk>
2026-02-20 15:32:19 +0000

    TemperatureSensor WS1

--- iot-portfolio-Patrick2Varcoe-main/prep/prep-ws1/src/TemperatureSensor.h
+++ iot-portfolio-Patrick2Varcoe-main/prep/prep-ws1/src/TemperatureSensor.h
@@ -12,14 +12,22 @@ public:
 
     //TODO Returns the ID of the sensor
     
-    int getId() const { return 0; }
+    int getId(){
+        return id;
+    };
+
 
     //TODO Returns the dimension (number of measurements) of the sensor
-    int getDimension() const { return 0; }
+    int getDimension() {
+        return dimension;
+    };
 
     //TODO Formats the sensor reading into a string. It needs to convert a vector of bytes to a string.
     virtual std::pair<std::string, std::string> format(std::vector<uint8_t> reading){ 
-       return { "", "" };
+           return {
+        std::to_string(reading[0]),
+        std::to_string(reading[1])
+        };
     };
 
 private:
