6f5c7aa3a59df9bd1ad8ee36f6b94efa56e8b3dd
Patrick2Varcoe <patrick2.varcoe@live.uwe.ac.uk>
2026-04-12 19:37:41 +0100

    tests

--- iot-portfolio-Patrick2Varcoe-main/prep/prep-ws1/tests/test_TemperatureSensor.cpp
+++ iot-portfolio-Patrick2Varcoe-main/prep/prep-ws1/tests/test_TemperatureSensor.cpp
@@ -5,15 +5,22 @@
 TEST_CASE("TemperatureSensor functionality", "[TemperatureSensor]") {
     
     //TODO: Create a TemperatureSensor object with ID 0 and dimension 1
-    
+    auto tsensor = std::make_shared<TemperatureSensor>(0,1);
 
     SECTION("Check ID and dimension") {
 
-    // TODO: Add assertions to check ID is 0 and dimension is 1.
-     
+        REQUIRE(tsensor.getId() == 0);
+        REQUIRE(tsensor.getDimension() == 1);
     }
+    // TODO: Add assertions to check ID is 0 and dimension is 1.
+    
+
 
     SECTION("Format reading") {
+        testData = {50,55,46,53,50};
+        auto formatted = tsensor->format(testData);
+        REQUIRE(formatted[0] == "Celsius") ;
+        REQUIRE(formatted[1] == "27.52")
            // TODO: Create a reading vector {50,55,46,53,50} ("27.52" in ASCII).
         // TODO: Call format(...) and assert the unit is "Celsius" and the value is "27.52".
     
