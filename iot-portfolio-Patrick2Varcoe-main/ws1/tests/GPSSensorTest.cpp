9972950e5de5539a7c0bf4ecc097cb61631fb8bf
Patrick2Varcoe <patrick2.varcoe@live.uwe.ac.uk>
2026-04-15 16:48:13 +0100

    t

---
+++ iot-portfolio-Patrick2Varcoe-main/ws1/tests/GPSSensorTest.cpp
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <cstdint>
+#include "GPSSensor.h"
+
+
+TEST_CASE("GPSSensor functionality", "[GPSSensor]") {
+    
+    
+    auto gsensor = std::make_shared<GPSSensor>(0);
+
+    SECTION("Check ID and dimension") {
+
+        REQUIRE(tsensor->getId() == 0);
+        REQUIRE(tsensor->getDimension() == 2);
+    }
+
+
+        SECTION("Format reading") {
+        std::vector<uint8_t> testData = {"51.457130;-2.557153"};
+        auto formatted = gsensor->format(testData);
+        REQUIRE(formatted.first == "lat:51.457130") ;
+        REQUIRE(formatted.second == "lon:-2.557153");
+
+    }
+}
