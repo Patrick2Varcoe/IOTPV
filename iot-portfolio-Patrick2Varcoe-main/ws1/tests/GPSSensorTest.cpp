c724c84cf8634fb843ee9e018d7975da92324441
Patrick2Varcoe <patrick2.varcoe@live.uwe.ac.uk>
2026-04-15 17:06:48 +0100

    l

--- iot-portfolio-Patrick2Varcoe-main/ws1/tests/GPSSensorTest.cpp
+++ iot-portfolio-Patrick2Varcoe-main/ws1/tests/GPSSensorTest.cpp
@@ -22,8 +22,8 @@ TEST_CASE("GPSSensor functionality", "[GPSSensor]") {
         std::string input = "51.457130;-2.557153";
         std::vector<uint8_t> testData(input.begin(), input.end());
         auto formatted = gsensor->format(testData);
-        REQUIRE(formatted.first == "lat:51.457130") ;
-        REQUIRE(formatted.second == "lon:-2.557153");
+        REQUIRE(formatted.first == "51.457130") ;
+        REQUIRE(formatted.second == "-2.557153");
 
     }
 }
